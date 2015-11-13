f77566ad56ede8735403dc42576ef774b31a2f8e
xdsking <xdsking@163.com>
2015-11-13 08:05:51 +0800

    add

--- admin/js/addMeeting.js
+++ admin/js/addMeeting.js
@@ -10,26 +10,29 @@ $(function () {
         $(".steepPane2").fadeOut(0);
         $(".steepPane3").fadeIn(500);
     });
-    $("#previousStep1").click(function(){
+    $("#previousStep1").click(function () {
         //TODO
     });
-    $("#previousStep2").click(function(){
+    $("#previousStep2").click(function () {
         $(".steepPane2").fadeOut(0);
         $(".steepPane1").fadeIn(500);
     });
-    $("#previousStep3").click(function(){
+    $("#previousStep3").click(function () {
         $(".steepPane3").fadeOut(0);
         $(".steepPane2").fadeIn(500);
     });
+    $(".cstm-panel-body").css("height", (document.body.clientHeight - 130) + "px");
     /*$(".cstm-glyphicon-add").mouseover(function(){
-        $(this).toggleClass("glyphicon-menu-right glyphicon-triangle-right");
-    }).mouseout(function(){
-        $(this).toggleClass("glyphicon-menu-right glyphicon-triangle-right");
-    });
-    $(".cstm-glyphicon-remove").mouseover(function(){
-        $(this).toggleClass("glyphicon-menu-left glyphicon-triangle-left");
-    }).mouseout(function(){
-        $(this).toggleClass("glyphicon-menu-left glyphicon-triangle-left");
-    });*/
-
+     $(this).toggleClass("glyphicon-menu-right glyphicon-triangle-right");
+     }).mouseout(function(){
+     $(this).toggleClass("glyphicon-menu-right glyphicon-triangle-right");
+     });
+     $(".cstm-glyphicon-remove").mouseover(function(){
+     $(this).toggleClass("glyphicon-menu-left glyphicon-triangle-left");
+     }).mouseout(function(){
+     $(this).toggleClass("glyphicon-menu-left glyphicon-triangle-left");
+     });*/
+    window.onbeforeunload=function(){
+        return confirm("确定要离开梦之都网站吗？");
+    }
 });
--- admin/js/index.js
+++ admin/js/index.js
@@ -2,8 +2,14 @@
  * Created by xuds on 2015/11/12.
  */
 $(function(){
+    var iframepage=$("#iframepage");
+    var thisLink="addMeeting";
     $(".menu .list-group-item").click(function(){
         $(".menu .list-group-item").removeClass("active");
         $(this).addClass("active");
+        if(thisLink!=$(this).attr("link")){
+            iframepage.attr("src",$(this).attr("link")+".html");
+            thisLink=$(this).attr("link");
+        }
     });
 });
